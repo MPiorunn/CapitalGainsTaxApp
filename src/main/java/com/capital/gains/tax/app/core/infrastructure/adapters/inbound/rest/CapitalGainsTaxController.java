23db3f04494d2a7a241ac2e80fec6f9aecaac307
Maciej Piorun <mpu@spyro-soft.com>
2021-06-13 22:54:53 +0200

    Removed unnecessary classes.

--- src/main/java/com/capital/gains/tax/app/core/infrastructure/adapters/inbound/rest/CapitalGainsTaxController.java
+++ src/main/java/com/capital/gains/tax/app/core/infrastructure/adapters/inbound/rest/CapitalGainsTaxController.java
@@ -1,6 +1,7 @@
 package com.capital.gains.tax.app.core.infrastructure.adapters.inbound.rest;
 
 import com.capital.gains.tax.app.core.domain.cgt.CapitalGainsTax;
+import com.capital.gains.tax.app.core.domain.cgt.CapitalGainsTaxFacade;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,10 +11,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 public class CapitalGainsTaxController {
 
-    private final CapitalGainsTaxService capitalGainsTaxService;
+    private final CapitalGainsTaxFacade capitalGainsTaxFacade;
 
     @GetMapping("/")
     public ResponseEntity<CapitalGainsTax> calculateCapitalGainsTax(CapitalGainsTaxRequest request) {
-        return ResponseEntity.ok(capitalGainsTaxService.calculateCapitalGainsTax(request));
+        return ResponseEntity.ok(capitalGainsTaxFacade.calculateCapitalGainsTax(request.getStock(),request.getAmount()));
     }
 }
