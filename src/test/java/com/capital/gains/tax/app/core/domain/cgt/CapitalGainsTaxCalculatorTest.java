6eab82774c851d4828146f98ea4b52e90b7809da
Maciej Piorun <mpu@spyro-soft.com>
2021-09-22 23:23:10 +0200

    Started working on main logic unit tests

---
+++ src/test/java/com/capital/gains/tax/app/core/domain/cgt/CapitalGainsTaxCalculatorTest.java
@@ -0,0 +1,34 @@
+package com.capital.gains.tax.app.core.domain.cgt;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import com.capital.gains.tax.app.core.domain.currency.CurrencyDataProvider;
+import com.capital.gains.tax.app.core.domain.dividend.DividendDataProvider;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+
+public class CapitalGainsTaxCalculatorTest {
+
+    @Mock
+    private DividendDataProvider dividendDataProvider;
+
+    @Mock
+    private CurrencyDataProvider currencyDataProvider;
+
+    private CapitalGainsTaxCalculator calculator;
+
+    @BeforeEach
+    public void setup() {
+        dividendDataProvider = Mockito.mock(DividendDataProvider.class);
+        currencyDataProvider = Mockito.mock(CurrencyDataProvider.class);
+        calculator = new CapitalGainsTaxCalculator(dividendDataProvider, currencyDataProvider);
+    }
+
+    @Test
+    public void xD() {
+        assertNotNull(calculator);
+    }
+
+}
